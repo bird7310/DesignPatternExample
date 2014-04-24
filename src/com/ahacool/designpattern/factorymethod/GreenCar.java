dda9c6f07a958bddecb1facc1b8e3cc5aa41cf04
Moto <motaotao@cvte.cn>
2014-04-24 22:23:39 +0800

    [Add Feature] Add 2 car classes.

---
+++ src/com/ahacool/designpattern/factorymethod/GreenCar.java
@@ -0,0 +1,14 @@
+package com.ahacool.designpattern.factorymethod;
+
+/**
+ * @ClassName GreenCar
+ * @Description TODO
+ * @author Moto
+ * @date 2014 2014-4-24
+ * 
+ */
+public class GreenCar {
+	public GreenCar() {
+		System.out.println("It is green car");
+	}
+}
---
+++ src/com/ahacool/designpattern/factorymethod/RedCar.java
@@ -0,0 +1,14 @@
+package com.ahacool.designpattern.factorymethod;
+
+/**
+ * @ClassName RedCar
+ * @Description TODO
+ * @author Moto
+ * @date 2014 2014-4-24
+ * 
+ */
+public class RedCar {
+	public RedCar() {
+		System.out.println("It is red car.");
+	}
+}
