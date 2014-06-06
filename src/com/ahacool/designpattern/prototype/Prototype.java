c78f92e126f678ff281054ae16a5ff0ddd8eb210
Moto <motaotao@cvte.cn>
2014-06-06 20:42:01 +0800

    [Add Feature] Add Prototype. Not done.

---
+++ src/com/ahacool/designpattern/prototype/Prototype.java
@@ -0,0 +1,17 @@
+package com.ahacool.designpattern.prototype;
+
+/**
+ * @ClassName Prototype
+ * @Description TODO
+ * @author Moto
+ * @date 2014 2014-6-6
+ * 
+ */
+public class Prototype implements Cloneable {
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+}
---
+++ src/com/ahacool/designpattern/prototype/test/PrototypeTest.java
@@ -0,0 +1,12 @@
+package com.ahacool.designpattern.prototype.test;
+
+/**
+ * @ClassName ProtoTypeTest
+ * @Description TODO
+ * @author Moto
+ * @date 2014 2014-6-6
+ *
+ */
+public class PrototypeTest {
+
+}
